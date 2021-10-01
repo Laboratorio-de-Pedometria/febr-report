57e5d9437423309a5c4d70ab795a09e03e2b51b8
Alessandro Samuel-Rosa <alessandrosamuelrosa@gmail.com>
2021-10-01 15:22:09 -0300

    relatório para reunião paranaense

--- code/Samuel-RosaEtAl2021a.R
+++ code/Samuel-RosaEtAl2021a.R
@@ -3,33 +3,59 @@
 # VII Reunião Paranaense de Ciência do Solo. Anais... In: VII REUNIÃO PARANAENSE DE CIÊNCIA DO SOLO.
 # Guarapuava, Paraná: Sociedade Brasileira de Ciência do Solo - Núcleo Estadual Paraná, 2021.
 
-# Carregar índice de conjuntos de dados publicados
-# Quantos conjuntos de dados publicados?
+# Carregar índice de conjuntos de dados publicados no FEBR
 febr_index <- data.table::fread("~/ownCloud/febr-repo/publico/febr-indice.txt")
+
+# Quantos conjuntos de dados já foram publicados?
 publicados <- nrow(febr_index)
 publicados
 
-# Carregar conjunto de dados padronizados e harmonizados
+# Carregar super conjunto de dados padronizados e harmonizados
 febr_dados <- data.table::fread("~/ownCloud/febr-repo/publico/febr-superconjunto.txt")
 head(febr_dados)
 
+# Quantas amostras já foram padronizadas e harmonizadas?
+amostras_brasil <- nrow(febr_dados)
+amostras_brasil
+
 # Quantos conjuntos de dados padronizados e harmonizados?
 padronizados <- length(unique(febr_dados[, dataset_id]))
-padronizados; padronizados / publicados
+padronizados; round(padronizados / publicados * 100)
 
-# Quantos eventos padronizados e harmonizados?
+# Quantos eventos já foram padronizados e harmonizados?
 eventos_brazil <- nrow(unique(febr_dados[, dataset_id, observacao_id]))
+eventos_brazil
 
-# Quantos eventos padronizados e harmonizados por estado?
+# Quantas amostras padronizadas e harmonizadas no Paraná?
 uf <- "PR"
 febr_estado <- febr_dados[estado_id == uf]
+amostras_parana <- nrow(febr_estado)
+amostras_parana; round(amostras_parana / amostras_brasil * 100)
+
+# Quantos eventos padronizados e harmonizados no Paraná?
 length(unique(febr_estado[, dataset_id]))
-febr_estado <- nrow(unique(febr_estado[, dataset_id, observacao_id]))
-febr_estado; febr_estado / eventos_brazil
+eventos_parana <- nrow(unique(febr_estado[, dataset_id, observacao_id]))
+eventos_parana; round(eventos_parana / eventos_brazil * 100)
 
-# 
-febr_data <- as.Date(febr_dados[estado_id == uf, observacao_data])
-datas <- as.Date(c("1900-01-01", "1960-01-01", "1990-01-01", "2010-01-01", "2020-01-01"))
-datas <- sapply(datas, function (x) sum(febr_data < x, na.rm = TRUE))
-datas <- diff(datas)
+# Quantas amostras não possuem data de coleta?
+data_faltante <- febr_dados[estado_id == uf & is.na(observacao_data), dataset_id, observacao_id]
+data_faltante <- nrow(unique(data_faltante))
+data_faltante; round(data_faltante / eventos_parana * 100)
 
+# Para as demais amostras, quando elas foram coletadas?
+data_presente <-
+  febr_dados[
+    estado_id == uf & !is.na(observacao_data),
+    c("dataset_id", "observacao_id", "observacao_data")
+  ]
+data_presente <- unique(data_presente)
+data_interesse <- as.Date(as.character(c(1900, seq(1960, 2020, 10))), format = "%Y")
+data_contagem <- sapply(data_interesse, function(x) sum(data_presente[, observacao_data] < x, na.rm = TRUE))
+data_contagem <- diff(data_contagem)
+names(data_contagem) <- data_interesse[-1]
+data_contagem; round(data_contagem / sum(data_contagem) * 100)
+
+# Quantos eventos não possuem coordenadas espaciais?
+coord_faltante <- febr_dados[estado_id == uf & is.na(coord_x), dataset_id, observacao_id]
+coord_faltante <- nrow(unique(febr_coord))
+coord_faltante; round(coord_faltante / eventos_parana * 100)
